fd876e7cca90b485f23e34763c18ca1a37e2b36e
Drew Hund <languageinvestigator@gmail.com>
2026-03-28 17:17:31 -0400

    scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 player_paddle = Paddle(350, 0)
 comp_paddle = Paddle(-350, 0)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -23,13 +25,28 @@ screen.onkey(fun=comp_paddle.move_down, key="s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # wall collision
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # paddle collision
+    if ((ball.distance(player_paddle) < 50 or ball.distance(comp_paddle) < 50) and
+            (ball.xcor() > 320 or ball.xcor() < -320)):
+        ball.bounce_x()
+
+    # out of bounds player
+    if ball.xcor() > 380:
+        scoreboard.comp_score()
+        ball.reset_position()
+
+    # left paddle misses
+    if ball.xcor() < -380:
+        scoreboard.player_score()
+        ball.reset_position()
 
 
 
